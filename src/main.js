0eb8bbe8ecf7f657a2d9eea6708dbace15a0d6b1
dmt <kb24amar@gmail.com>
2021-03-30 22:51:48 -0500

    first commit (AHarris)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false;
+
+window.bus = new Vue();
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+  <div class="main-content">
+    <CustomerList />
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import CustomerList from '@/components/CustomerList.vue'
 export default {
-  name: "Home",
+  name: 'home',
   components: {
-    HelloWorld,
-  },
-};
+    CustomerList
+  }
+}
+
 </script>

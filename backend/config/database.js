568f200c6a77a7f83ba6cedbd1976903c864f346
dmt <kb24amar@gmail.com>
2021-04-08 21:50:27 -0500

    added profileform

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,9 @@ import mysql from "mysql2";
   
 // create the connection to database
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'MumboJumbo#9',
+  host: 'CoT-CIS4375-03',
+  user: 'admin',
+  password: 'DMT@uh2021',
   database: '2NPortal'
 });
  
